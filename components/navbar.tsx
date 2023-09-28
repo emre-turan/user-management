b155df1d74f04243118ee787ddb7f5399e380a13
Emre Turan <itsemreturan@gmail.com>
2023-09-28 21:13:40 +0300

    improved protect route logic

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
 import Image from "next/image";
-import Logo from "../public/logo.png";
+import Logo from "../public/SF Pro Bold Light - v2.svg";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import UserActions from "./user-actions";
@@ -13,11 +13,16 @@ const Navbar = async () => {
   const userRole: Role = (session?.user?.role as Role) || "guest";
   console.log("User Role:", userRole);
   return (
-    <div className="fixed top-0 z-10 w-full border-b border-s-zinc-200 bg-zinc-100 py-2">
+    <div className="fixed top-0 z-10 w-full border-b border-s-zinc-200 bg-zinc-100 py-6">
       <div className="container flex items-center justify-between">
-        <Link href="/">
-          <Image alt="" src={Logo} />
-        </Link>
+        <div className="absolute">
+          <div className="relative h-28 w-28">
+            <Link href="/">
+              <Image alt="application logo" src={Logo} />
+            </Link>
+          </div>
+        </div>
+
         <div className="ml-auto">
           {session?.user ? (
             <UserActions role={userRole} />
@@ -29,10 +34,7 @@ const Navbar = async () => {
               >
                 Sign in
               </Link>
-              <Link
-                className={buttonVariants()}
-                href="/sign-up"
-              >
+              <Link className={buttonVariants()} href="/sign-up">
                 Sign up
               </Link>
             </div>
