70d44af1a832f4829912ec75dd0a16152c86c3d6
Emre Turan <itsemreturan@gmail.com>
2023-10-09 23:00:30 +0300

    code readability improvements

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,37 +7,43 @@ import { authOptions } from "@/lib/auth";
 import UserActions from "./user-actions";
 import { Role } from "@/types/role";
 
+const AppLogo = () => {
+  return (
+    <div className="absolute">
+      <div className="relative h-28 w-28">
+        <Link href="/">
+          <Image alt="application logo" src={Logo} />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+const SignInOrSignUpButtons = () => {
+  return (
+    <div className="flex space-x-4">
+      <Link className={buttonVariants({ variant: "link" })} href="/sign-in">
+        Sign in
+      </Link>
+      <Link className={buttonVariants()} href="/sign-up">
+        Sign up
+      </Link>
+    </div>
+  );
+};
+
 const Navbar = async () => {
   const session = await getServerSession(authOptions);
-  console.log("Session Data:", session);
   const userRole: Role = (session?.user?.role as Role) || "guest";
-  console.log("User Role:", userRole);
   return (
     <div className="fixed top-0 z-10 w-full border-b border-s-zinc-200 bg-zinc-100 py-6">
       <div className="container flex items-center justify-between">
-        <div className="absolute">
-          <div className="relative h-28 w-28">
-            <Link href="/">
-              <Image alt="application logo" src={Logo} />
-            </Link>
-          </div>
-        </div>
-
+        <AppLogo />
         <div className="ml-auto">
           {session?.user ? (
             <UserActions role={userRole} />
           ) : (
-            <div className="flex space-x-4">
-              <Link
-                className={buttonVariants({ variant: "link" })}
-                href="/sign-in"
-              >
-                Sign in
-              </Link>
-              <Link className={buttonVariants()} href="/sign-up">
-                Sign up
-              </Link>
-            </div>
+            <SignInOrSignUpButtons />
           )}
         </div>
       </div>
