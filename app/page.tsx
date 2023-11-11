12c67dde282ad27f481f7c993e95d4b9ace0a195
Emre Turan <itsemreturan@gmail.com>
2023-11-11 18:05:09 +0300

    updated UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import UserCard from "@/components/user-card";
+import { FadeIn } from "@/components/fade-in";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
@@ -10,33 +11,35 @@ export default async function Home() {
   const users = await res.json();
 
   return (
-    <main className="container mx-auto mt-28 max-w-7xl px-6 lg:px-8">
-      <div className="p-2">
-        <h1 className="mb-2 text-2xl font-semibold">
-          Welcome to User Management App
-        </h1>
-        <h2 className="mb-4 text-sm text-muted-foreground">
-          The application allows users to edit their own profiles, view other
-          users&apos; profiles, and provides admin functionalities to manage all
-          users. Developed with modern technologies and best practices, this
-          project serves as a comprehensive example of a full-stack application
-          to demonstrate my technical skills and experience.
-        </h2>
-      </div>
+    <FadeIn>
+      <main className="container mx-auto mt-28 max-w-7xl px-6 lg:px-8">
+        <div className="p-2">
+          <h1 className="mb-2 text-2xl font-semibold">
+            Welcome to User Management App
+          </h1>
+          <h2 className="mb-4 text-sm text-muted-foreground">
+            The application allows users to edit their own profiles, view other
+            users&apos; profiles, and provides admin functionalities to manage
+            all users. Developed with modern technologies and best practices,
+            this project serves as a comprehensive example of a full-stack
+            application to demonstrate my technical skills and experience.
+          </h2>
+        </div>
 
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
-        {users.map(
-          (user: {
-            id: string;
-            name: string;
-            surname: string;
-            planet: string;
-            email: string;
-          }) => (
-            <UserCard key={user.id} {...user} />
-          ),
-        )}
-      </div>
-    </main>
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
+          {users.map(
+            (user: {
+              id: string;
+              name: string;
+              surname: string;
+              planet: string;
+              email: string;
+            }) => (
+              <UserCard key={user.id} {...user} />
+            ),
+          )}
+        </div>
+      </main>
+    </FadeIn>
   );
 }
